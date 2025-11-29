36626e677b396d16ae5f70609bb03e5c23f29235
Amit Kumar <0001amit.kumar1@gmail.com>
2025-11-29 11:47:05 +0530

    first commit

---
+++ src/LaravelDeskslipServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Vedmarg\Core;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelDeskslipServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // Merge config
+        $this->mergeConfigFrom(__DIR__.'/../config/vedmarg.php', 'vedmarg');
+    }
+
+    public function boot()
+    {
+        // Routes
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+
+        // Views
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'deskslip');
+
+        // Publish config
+        $this->publishes([
+            __DIR__.'/../config/deskslip.php' => config_path('deskslip.php'),
+        ], 'deskslip-config');
+
+        // Publish views
+        $this->publishes([
+            __DIR__.'/../resources/views' => resource_path('views/vendor/deskslip'),
+        ], 'deskslip-views');
+    }
+}
